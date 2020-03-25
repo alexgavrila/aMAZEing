f0bd753bbc531d934a582266c01b741823915e66
cristinaistudor <cristinaistudor31@gmail.com>
2020-03-25 17:32:18 +0200

    Generare Labirint

---
+++ Assets/MazeConfig/Scripts/MazeCellEdge.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MazeCellEdge : MonoBehaviour
+{
+
+    public MazeCell cell, otherCell;
+
+    public MazeDirection direction;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public virtual void Initialize(MazeCell cell, MazeCell otherCell, MazeDirection direction)
+    {
+        this.cell = cell;
+        this.otherCell = otherCell;
+        this.direction = direction;
+        cell.SetEdge(direction, this);
+        transform.parent = cell.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = direction.ToRotation();
+    }
+
+
+}
