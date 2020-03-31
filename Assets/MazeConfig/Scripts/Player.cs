53cb1fde458d6392d8c8426c17c1ddf5fab6520c
alexgavrila <email@alexgavrila.ro>
2020-04-01 00:40:26 +0300

    Player Movement

--- Assets/MazeConfig/Scripts/Player.cs
+++ Assets/MazeConfig/Scripts/Player.cs
@@ -8,55 +8,75 @@ public class Player : MonoBehaviour
 
 	private MazeDirection currentDirection;
 
+	private CharacterController _controller;
+
+
+	// movement
+	public float speed = 2.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+	// camera
+    public float mouseSensitivity = 2.0f;
+    float xAxisClamp = 1.0f;
+
+	void Start()
+	{
+         _controller = GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		MovePlayer();
+
+		RotateCamera();
+	}
+
+	void MovePlayer()
+	{
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		_controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * mouseSensitivity;
+        float rotAmountY = mouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = Camera.main.transform.rotation.eulerAngles;
+        Vector3 targetRotBody = transform.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if(xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if(xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+
+        Camera.main.transform.rotation = Quaternion.Euler(targetRotCam);
+        transform.rotation = Quaternion.Euler(targetRotBody);
+    }
+
 	public void SetLocation(MazeCell cell)
 	{
 		currentCell = cell;
-		transform.localPosition = cell.transform.localPosition;
-	}
-
-	private void Move(MazeDirection direction)
-	{
-		MazeCellEdge edge = currentCell.GetEdge(direction);
-		if (edge is MazePassage)
-		{
-			SetLocation(edge.otherCell);
-		}
-	}
-
-	
-
-	private void Look(MazeDirection direction)
-	{
-		transform.localRotation = direction.ToRotation();
-		currentDirection = direction;
-	}
-
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			Move(currentDirection);
-		}
-		else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			Move(currentDirection.GetNextClockwise());
-		}
-		else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-		{
-			Move(currentDirection.GetOpposite());
-		}
-		else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			Move(currentDirection.GetNextCounterclockwise());
-		}
-		else if (Input.GetKeyDown(KeyCode.Q))
-		{
-			Look(currentDirection.GetNextCounterclockwise());
-		}
-		else if (Input.GetKeyDown(KeyCode.E))
-		{
-			Look(currentDirection.GetNextClockwise());
-		}
+		Vector3 pos = cell.transform.localPosition;
+		transform.localPosition = pos;
 	}
 
 }
